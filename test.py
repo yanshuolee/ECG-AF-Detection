acddb9fcdb1641f03612d1685636cb8aec4fa55d
Yanshuo Lee <33903370+yanshuolee@users.noreply.github.com>
2018-05-01 20:41:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,18 +10,22 @@ import data_datacut as dp
 newData, newLabel = dp.datacut(9,300,10).newdata()
 # print(newData.shape)
 # print(newLabel.shape)
-shape = (newData.shape[0] ,newData.shape[1])
-newData = np.reshape(newData, (newData.shape[0], newData.shape[0] ,newData.shape[1]))
-print(newData.shape)
 
-model = Sequential() 
-model.add(Conv1D(filters = 512, kernel_size = 3, input_shape = shape, activation = "relu"))
-model.add(MaxPooling1D(pool_size = 100))
-model.add(Dense(4, activation = "softmax"))
-print(model.summary())
+for i in newData:
+  if len(i) != 2700:
+    print(len(i))
+# shape = (newData.shape[0] ,newData.shape[1])
+# newData = np.reshape(newData, (newData.shape[0], newData.shape[0] ,newData.shape[1]))
+# print(newData.shape)
 
-model.compile(optimizer = "adam", loss = "categorical_crossentropy", matrics = ["accuracy"])
-model.fit(x = newData, y = newLabel, validation_split=0.2, epochs=10, batch_size=100, verbose=2)
+# model = Sequential() 
+# model.add(Conv1D(filters = 512, kernel_size = 3, input_shape = shape, activation = "relu"))
+# model.add(MaxPooling1D(pool_size = 100))
+# model.add(Dense(4, activation = "softmax"))
+# print(model.summary())
+
+# model.compile(optimizer = "adam", loss = "categorical_crossentropy", matrics = ["accuracy"])
+# model.fit(x = newData, y = newLabel, validation_split=0.2, epochs=10, batch_size=100, verbose=2)
 
 
 
