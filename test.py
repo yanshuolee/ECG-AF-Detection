ba1633055a35f3f71002a55372f7f78ed655e4f2
Yanshuo Lee <33903370+yanshuolee@users.noreply.github.com>
2018-05-01 18:22:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,21 +8,18 @@ np.set_printoptions(suppress=True)
 
 import data_datacut as dp
 newData, newLabel = dp.datacut(9,300,10).newdata()
-print(newData.shape)
-print(newLabel.shape)
+# print(newData.shape)
+# print(newLabel.shape)
 
 
-# model = Sequential() 
-# model.add(Conv1D(filters = 512, kernel_size = 3, input_shape = (1000, 10, 2700), activation = "relu"))
-# model.add(MaxPooling1D(pool_size = 100))
-# model.add(Dense(4, activation = "softmax"))
+model = Sequential() 
+model.add(Conv1D(filters = 512, kernel_size = 3, input_shape = (None, 2700), activation = "relu"))
+model.add(MaxPooling1D(pool_size = 100))
+model.add(Dense(4, activation = "softmax"))
+print(model.summary())
 
-# print(model.summary())
-
-
-
-# model.compile(optimizer = "adam", loss = "categorical_crossentropy", matrics = ["accuracy"])
-# model.fit(x = newData, y = newLabel, validation_split=0.2, epochs=10, batch_size=100, verbose=2)
+model.compile(optimizer = "adam", loss = "categorical_crossentropy", matrics = ["accuracy"])
+model.fit(x = newData, y = newLabel, validation_split=0.2, epochs=10, batch_size=100, verbose=2)
 
 
 
