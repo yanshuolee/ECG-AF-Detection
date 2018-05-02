d53cdf792f1e256edee6a5c4caa79f370b4c2722
Yanshuo Lee <33903370+yanshuolee@users.noreply.github.com>
2018-05-02 10:05:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ trainData = newData.reshape(newData.shape[0], newData.shape[1] ,1)
 print(trainData.shape)
 
 model = Sequential() 
-model.add(Conv1D(filters = 512, kernel_size = 3, input_shape = (newData.shape[0], newData.shape[1] ,1), activation = "relu"))
+model.add(Conv1D(filters = 512, kernel_size = 3, input_shape = (newData.shape[1] ,1), activation = "relu"))
 model.add(MaxPooling1D(pool_size = 100))
 # model.add(Dense(4, activation = "softmax"))
 print(model.summary())
