bc60907695f971cddccee0b2b1fd2abaad4cac35
Yanshuo Lee <33903370+yanshuolee@users.noreply.github.com>
2018-05-04 17:02:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,17 +10,17 @@ import data_datacut as dp
 newData, newLabel = dp.datacut(9,300,10).newdata()
 
 
-# trainData = newData.reshape((newData.shape[0], newData.shape[1] ,1))
+trainData = newData.reshape((newData.shape[0], 1newData.shape[1]))
 
 
 model = Sequential() 
-model.add(Conv1D(filters = 512, kernel_size = 3, input_shape = (2700,), activation = "relu"))
+model.add(Conv1D(filters = 512, kernel_size = 3, input_shape = (2700,1), activation = "relu"))
 # model.add(MaxPooling1D(pool_size = 100))
 # model.add(Dense(4, activation = "softmax"))
 print(model.summary())
 
 model.compile(optimizer = "adam", loss = "categorical_crossentropy", matrics = ["accuracy"])
-model.fit(x = newData, y = newLabel, validation_split=0.2, epochs=10, batch_size=100, verbose=2)
+model.fit(x = trainData, y = newLabel, validation_split=0.2, epochs=10, batch_size=100, verbose=2)
 
 
 
