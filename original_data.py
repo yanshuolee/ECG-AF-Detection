f8252265dee864249624d2b425ceb60400b8f301
Yanshuo Lee <33903370+yanshuolee@users.noreply.github.com>
2018-05-07 15:34:48 +0800

    Update original_data.py

--- original_data.py
+++ original_data.py
@@ -29,11 +29,11 @@ class makeData():
         numOfAf, numOfNormal, numOfOther, numOfNoise = self.numOfDataForTraining()
         self.startMakingData(numOfAf, numOfNormal, numOfOther, numOfNoise)
         
-        self.trainingData = np.array(self.trainingData)
-        self.trainingData = np.array(self.trainingData)
-        self.trainingData = np.array(self.trainingData)
-        self.trainingData = np.array(self.trainingData)
-        return self.trainingData, self.traingLabel, self.testData, self.testLabel 
+        trainingData = np.array(self.trainingData)
+        trainingData = np.array(self.trainingData)
+        trainingData = np.array(self.trainingData)
+        trainingData = np.array(self.trainingData)
+        return trainingData, traingLabel, testData, testLabel 
 
     def numOfDataForTraining(self):
         
