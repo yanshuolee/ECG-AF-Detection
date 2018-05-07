c533763745715932f4c311cc2fe33ab877ea270d
Yanshuo Lee <33903370+yanshuolee@users.noreply.github.com>
2018-05-07 16:39:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -12,27 +12,31 @@ import data_preprocessing as dp
 
 tmp = dp.makeData(9, 0.7)
 trainD, trainL, testD, testL = tmp.main()
-print(trainD.shape)
-print(trainL.shape)
-print(testD.shape)
-print(testL.shape)
+# print(trainD.shape)
+# print(trainL.shape)
+# print(testD.shape)
+# print(testL.shape)
 
-# trainData = newData.reshape((newData.shape[0], newData.shape[1], 1))
-# trainLabel = np_utils.to_categorical(newLabel, 4)
+trainData = trainD.reshape((trainD.shape[0], trainD.shape[1], 1))
+trainLabel = np_utils.to_categorical(trainL, 4)
+testData = testD.reshape((testD.shape[0], testD.shape[1], 1))
+testLabel = np_utils.to_categorical(testL, 4)
 
-# print('Train data shape:' , trainData.shape)
-# print('Train label shape:', trainLabel.shape)
+print(trainData.shape)
+print(trainLabel.shape)
+print(testData.shape)
+print(testLabel.shape)
 
 
-# model = Sequential() 
-# model.add(Conv1D(filters = 512, kernel_size = 3, input_shape = (newData.shape[1], 1), activation = "relu"))
-# model.add(MaxPooling1D(pool_size = 100))
-# model.add(Flatten())
-# model.add(Dense(4, activation = "softmax"))
-# print(model.summary())
+model = Sequential() 
+model.add(Conv1D(filters = 512, kernel_size = 3, input_shape = (newData.shape[1], 1), activation = "relu"))
+model.add(MaxPooling1D(pool_size = 100))
+model.add(Flatten())
+model.add(Dense(4, activation = "softmax"))
+print(model.summary())
 
-# model.compile(optimizer ='adam', loss = "categorical_crossentropy", metrics=['accuracy'])
-# model.fit(x = trainData, y = trainLabel, epochs=10, batch_size=100, verbose=2)
+model.compile(optimizer ='adam', loss = "categorical_crossentropy", metrics=['accuracy'])
+model.fit(x = trainData, y = trainLabel, epochs=10,validation_split=0.2, batch_size=100, verbose=2)
 
 
 
