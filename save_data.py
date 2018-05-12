56e404f5d26bf27f3af890acffbc650c431c2e33
yanshuo <yanshuo@localhost.localdomain>
2018-05-12 10:19:47 +0800

    revised

--- save_data.py
+++ save_data.py
@@ -4,11 +4,11 @@ import pandas as pd
 np.set_printoptions(suppress=True)
 import data_preprocessing as dp
 
-tmp = dp.makeData(9, 0.5, 0.2, 0.3, overlap_dot=1350)
-trainD, trainL, valD, valL, testD, testL = tmp.main()
+to_9_overlapping = dp.makeData(9, 0.5, 0.2, 0.3, overlap_dot=1350)
+trainD, trainL, valD, valL, testD, testL = to_9_overlapping.main()
 
-tmp = dp.makeData(9, 0.5, 0.2, 0.3)
-trainD, trainL, valD, valL, testD, testL = tmp.main()
+to_9 = dp.makeData(9, 0.5, 0.2, 0.3)
+trainD, trainL, valD, valL, testD, testL = to_9.main()
 
-tmp = dp.makeData(30, 0.5, 0.2, 0.3)
-trainD, trainL, valD, valL, testD, testL = tmp.main()
+to_30 = dp.makeData(30, 0.5, 0.2, 0.3)
+trainD, trainL, valD, valL, testD, testL = to_30.main()
